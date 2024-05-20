d2bbd930fb6e8033403b1d6c4e7f730de9c15731
Kavivarshni M <87589645+kavivarshni@users.noreply.github.com>
2024-05-20 18:37:13 +0530

    Add files via upload

---
+++ day01/BooleanConversion.js
@@ -0,0 +1,21 @@
+//Numeric Conversion
+let a=null;
+let b=undefined;
+let c=true;
+let d=false;
+let e="Hi how r u";  
+let f="";          //Nothing is there so, false
+
+let res1=Boolean(a);
+let res2=Boolean(b);
+let res3=Boolean(c);
+let res4=Boolean(d);
+let res5=Boolean(e);
+let res6=Boolean(f);
+
+console.log(res1);
+console.log(res2);
+console.log(res3);
+console.log(res4);
+console.log(res5);
+console.log(res6);

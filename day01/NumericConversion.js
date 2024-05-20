088c5a4c05e40435121b4e1f314b0ebac81e63fd
Kavivarshni M <87589645+kavivarshni@users.noreply.github.com>
2024-05-20 18:35:26 +0530

    Add files via upload

---
+++ day01/NumericConversion.js
@@ -0,0 +1,18 @@
+//Numeric Conversion
+let a=null;
+let b=undefined;
+let c=true;
+let d=false;
+let e="Hi how r u";
+
+let res1=Number(a);
+let res2=Number(b);
+let res3=Number(c);
+let res4=Number(d);
+let res5=Number(e);
+
+console.log(res1);
+console.log(res2);
+console.log(res3);
+console.log(res4);
+console.log(res5);

5540873d9f689b9653e51fc6fcca84bd3e4ecc2f
Kavivarshni M <87589645+kavivarshni@users.noreply.github.com>
2024-05-20 18:34:59 +0530

    Add files via upload

---
+++ day01/typeof.js
@@ -0,0 +1,9 @@
+let isalive=true;
+let age;
+age=null;
+console.log(typeof(true));
+console.log(typeof(Math));
+console.log(typeof(null));
+console.log(typeof(isalive));
+console.log(typeof(age));
+console.log(typeof(0.5));

0ede73ad8eab8dd2d702eba14529c10e7d676cc0
Kavivarshni M <87589645+kavivarshni@users.noreply.github.com>
2024-05-20 18:34:00 +0530

    Add files via upload

---
+++ day01/incrementdecrement.js
@@ -0,0 +1,55 @@
+console.log("Pre Increment");
+let x=10;
+let res=++x;
+console.log(res);
+
+console.log("Post Increment");
+let y=10;
+let res1=y++;
+console.log(res1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

2cabbc1064d2df10167e92cce5b0dd7d3122e96a
Kavivarshni M <87589645+kavivarshni@users.noreply.github.com>
2024-05-20 18:37:43 +0530

    Add files via upload

---
+++ day01/Stringtype.js
@@ -0,0 +1,19 @@
+let company="Iexceed";
+let message=`Welcome to ${company}`;    //using backtick
+console.log(message);
+let isalive=false;
+if(isalive==true)
+{
+    console.log("Working");
+}
+else
+{
+    console.log("Not Working ");
+}
+
+//diff b/w Null & Undefined
+let age;            //undefined it is default when we didn't  assigned anything 
+let age1=null;
+//wecan also assign null values
+console.log(age);
+console.log(age1);

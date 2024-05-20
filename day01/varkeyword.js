f006e844726f691e13b604aee36c315a917f05a8
Kavivarshni M <87589645+kavivarshni@users.noreply.github.com>
2024-05-20 18:34:39 +0530

    Add files via upload

---
+++ day01/varkeyword.js
@@ -0,0 +1,11 @@
+x=10;
+X=20;                     //Case Sensitive
+var y="Kavivarshni";
+console.log(y);
+var y="Ranjeeth";   
+function myname()
+{
+    console.log(y);
+}
+    //var Keyword can be reassigned
+myname();

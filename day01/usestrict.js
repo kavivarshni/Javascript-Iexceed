236c3d1dc6e38802baab3a0364c86ff9ccc67ff4
Kavivarshni M <87589645+kavivarshni@users.noreply.github.com>
2024-05-20 18:35:55 +0530

    Add files via upload

---
+++ day01/usestrict.js
@@ -0,0 +1,16 @@
+//"use strict";
+var x=10;
+console.log(x);
+//we cannot declare variable without mentioning any datatype
+function multiplication(x,x)            //we cannot declare variable without mentioning any datatype
+{
+    console.log(x*x);
+}
+let y=2**53;
+console.log(y);
+console.log(y+1);
+console.log(y+2);
+console.log(y+3);
+console.log(y+4);
+
+multiplication(6,5);
